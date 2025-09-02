4e7b78e32e28b792d1f2e8caa059fadd3a19c0f2
Bibul6717 <bibul.dhiman@zamplia.com>
2025-09-02 10:30:53 +0530

    Update databaseSetupMysql.js

--- src/archiveData/database/databaseSetupMysql.js
+++ src/archiveData/database/databaseSetupMysql.js
@@ -1,16 +1,4 @@
 
-// const mysql = require('mysql');
-
-//   pool  = mysql.createPool({
-//   connectionLimit : 50,
-//   host: "zampliaarchiveddb.mysql.database.azure.com",  // ip address of server running mysql
-//   port:  "3306",
-//   user: "zampliaarchived@zampliaarchiveddb",    // user name to your mysql database
-//   password: "Zamplia@Pr0jectX!",
-//   database: "zampliaarchiveddb", // use the specified database
-//   multipleStatements: true,
-//   ssl: true
-// });
 
 var mysql = require('mysql');
 var config;
