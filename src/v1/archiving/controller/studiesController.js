0108ab15a874ca44d7c5d9bba34e3514599474bc
Bibul6717 <bibul.dhiman@zamplia.com>
2025-09-09 11:07:29 +0530

    inprogress

--- src/v1/archiving/controller/studiesController.js
+++ src/v1/archiving/controller/studiesController.js
@@ -1,24 +1,64 @@
-const { getStudiesForArchiving, getStudiesMappingForArchiving, getStudiesDemoMappingForArchiving, getStudiesDemoAgeMappingForArchiving, getConstraintsForArchiving, getConstraintsDemoMappingForArchiving, getStudiesStatusCountForArchiving, getStudiesStatusCountOnVendorsForArchiving } = require('../dao');
+const { getStudiesForArchiving, getStudiesMappingForArchiving, getStudiesDemoMappingForArchiving, getStudiesDemoAgeMappingForArchiving, getConstraintsForArchiving, getConstraintsDemoMappingForArchiving, getStudiesStatusCountForArchiving, getStudiesStatusCountOnVendorsForArchiving, insertStudiesArchive, get_studies_already_archived, insertStudyDemoArchive, insertStudyDemoAgeArchive, insertConstraintsArchive, insertDemoConstraintsArchive, insertStudiesStatusCountArchive, insertMappingArchive, updateArchivedFlagInStudies, insertStudiesStatusCountOnVendorsArchive} = require('../dao');
+const { deleteStudiesForArchiving } = require('../utillisForDeleteData');
 
 
 async function archivingStudiesData(req, res) {
    try {
 
     let limit = req.query.limit ? parseInt(req.query.limit) : 1000;
-    let studies_For_Archive = await getStudiesForArchiving(limit);
-    let s_id = studies_For_Archive.map(d => d._id);
-    let mapping_For_Archive = await getStudiesMappingForArchiving(s_id);
-    let studydemo_For_Archive = await getStudiesDemoMappingForArchiving(s_id);
-    let studydemo_age_For_Archive = await getStudiesDemoAgeMappingForArchiving(s_id);
-    let constraints_For_Archive = await getConstraintsForArchiving(s_id);
-    let demo_constraints_For_Archive = await getConstraintsDemoMappingForArchiving(s_id);
-    let studies_status_count_For_Archive = await getStudiesStatusCountForArchiving(s_id);
-    let studies_status_count_on_vendors_For_Archive = await getStudiesStatusCountOnVendorsForArchiving(s_id);
+    let studies_For_Archive = await getStudiesForArchiving(limit);  //done
+    let _id = studies_For_Archive.map(d => d._id);
+    let studies_already_archived = await get_studies_already_archived(_id);
+     let _id_for_delete = studies_already_archived.map(d => d._id); 
+    let mapping_For_Archive = await getStudiesMappingForArchiving(_id);   // done
+    let studydemo_For_Archive = await getStudiesDemoMappingForArchiving(_id); // done
+    let studydemo_age_For_Archive = await getStudiesDemoAgeMappingForArchiving(_id);
+    let constraints_For_Archive = await getConstraintsForArchiving(_id);
+    let demo_constraints_For_Archive = await getConstraintsDemoMappingForArchiving(_id);
+    let studies_status_count_For_Archive = await getStudiesStatusCountForArchiving(_id);
+    let studies_status_count_on_vendors_For_Archive = await getStudiesStatusCountOnVendorsForArchiving(_id);
+
+    if(studies_already_archived.length){
+        await deleteStudiesForArchiving(_id_for_delete);
+    }
+
+    if(studies_For_Archive.length){
+        await insertStudiesArchive(studies_For_Archive);
+    }
     
+    if(mapping_For_Archive.length){
+        await insertMappingArchive(mapping_For_Archive);
+    }
 
+    if(studydemo_For_Archive.length){
+        await insertStudyDemoArchive(studydemo_For_Archive);
+    }
 
+    if(studydemo_age_For_Archive.length){
+        await insertStudyDemoAgeArchive(studydemo_age_For_Archive);
+    }
+    
+    if(constraints_For_Archive.length){
+        await insertConstraintsArchive(constraints_For_Archive);
+    }
 
+    if(demo_constraints_For_Archive.length){
+        await insertDemoConstraintsArchive(demo_constraints_For_Archive);
+    }
 
+    if(studies_status_count_For_Archive.length){
+        await insertStudiesStatusCountArchive(studies_status_count_For_Archive);
+    }
+
+    if(studies_status_count_on_vendors_For_Archive.length){
+        await insertStudiesStatusCountOnVendorsArchive(studies_status_count_on_vendors_For_Archive);
+    }
+ 
+     await updateArchivedFlagInStudies(_id);
+
+     return;
+    
+     
 
 } catch (errno) {
     return
--- src/v1/archiving/dao.js
+++ src/v1/archiving/dao.js
@@ -1,11 +1,18 @@
+const { update } = require('lodash');
 const { executeDev7 } = require('../../database/queryWrapperMysql');
 
 async function getStudiesForArchiving(limit) {
-    let query = 'select _id, s.* from studies as s where s.apiType=1 and  s.createdAt < DATE_SUB(NOW(), INTERVAL 2 MONTH) LIMIT ?';
+    let query = 'select  s.* from studies as s where s.apiType=1 and  s.createdAt < DATE_SUB(NOW(), INTERVAL 2 MONTH) LIMIT ?';
     let result = await executeDev7(query, [limit]);
     return result;
 }
 
+async function get_studies_already_archived(sids) {
+    let query = 'select _id  from studies_backup as s where s._id in (?)';
+    let result = await executeDev7(query, [sids]);
+    return result;
+}
+    
 async function getStudiesMappingForArchiving(sids) {
     let query = 'select * from mappings where studyId in(?)';
     let result = await executeDev7(query, [sids]);
@@ -48,6 +55,161 @@ async function getStudiesStatusCountOnVendorsForArchiving(sids) {
     return result;
 }
 
+async function insertStudiesArchive(studies) {
+    if (!studies || studies.length === 0) return;
+
+    const columns = Object.keys(studies[0]);
+   
+    const placeholders = studies
+        .map(() => `(${columns.map(() => '?').join(',')})`)
+        .join(',');
+    const values = studies.flatMap(study => columns.map(col => study[col]));
+    const query = `INSERT INTO studies_backup (${columns.join(',')}) VALUES ${placeholders}`;
+    let data =  await executeDev7(query, values);
+    return 
+}
+
+async function insertMappingArchive(mappings) {
+    if (!mappings || mappings.length === 0) return;
+
+    // Exclude auto_increment key m_id
+    const columns = Object.keys(mappings[0]).filter(col => col !== 'm_id');
+
+    const placeholders = mappings
+        .map(() => `(${columns.map(() => '?').join(',')})`)
+        .join(',');
+
+    const values = mappings.flatMap(map => columns.map(col => map[col]));
+
+    const query = `INSERT INTO mappings_backup (${columns.join(',')}) VALUES ${placeholders}`;
+
+    let data = await executeDev7(query, values);
+    return data;
+}
+
+async function insertStudyDemoArchive(studyDemos) {
+    if (!studyDemos || studyDemos.length === 0) return;
+
+    // Exclude auto_increment key `id`
+    const columns = Object.keys(studyDemos[0]).filter(col => col !== 'id');
+
+    const placeholders = studyDemos
+        .map(() => `(${columns.map(() => '?').join(',')})`)
+        .join(',');
+
+    const values = studyDemos.flatMap(sd => columns.map(col => sd[col]));
+
+    const query = `INSERT INTO studydemomapping_backup (${columns.join(',')}) VALUES ${placeholders}`;
+
+    let data = await executeDev7(query, values);
+    return data;
+}
+
+async function insertStudyDemoAgeArchive(studyDemoAges) {
+    if (!studyDemoAges || studyDemoAges.length === 0) return;
+
+    // Exclude auto_increment key `id`
+    const columns = Object.keys(studyDemoAges[0]).filter(col => col !== 'id');
+
+    const placeholders = studyDemoAges
+        .map(() => `(${columns.map(() => '?').join(',')})`)
+        .join(',');
+
+    const values = studyDemoAges.flatMap(sd => columns.map(col => sd[col]));
+
+    const query = `INSERT INTO demoagemapping_backup (${columns.join(',')}) VALUES ${placeholders}`;
+
+    let data = await executeDev7(query, values);
+    return data;
+}
+
+async function insertConstraintsArchive(constraints) {
+    if (!constraints || constraints.length === 0) return;
+
+    // Exclude auto_increment key `id`
+    const columns = Object.keys(constraints[0]).filter(col => col !== 'id');
+
+    const placeholders = constraints
+        .map(() => `(${columns.map(() => '?').join(',')})`)
+        .join(',');
+
+    const values = constraints.flatMap(c => columns.map(col => c[col]));
+
+    const query = `INSERT INTO constrainsts_backup (${columns.join(',')}) VALUES ${placeholders}`;
+
+    let data = await executeDev7(query, values);
+    return data;
+}
+
+async function insertDemoConstraintsArchive(demoConstraints) {
+    if (!demoConstraints || demoConstraints.length === 0) return;
+
+    // Exclude `id` to avoid duplicate PK errors
+    const columns = Object.keys(demoConstraints[0]).filter(col => col !== 'id');
+
+    const placeholders = demoConstraints
+        .map(() => `(${columns.map(() => '?').join(',')})`)
+        .join(',');
+
+    const values = demoConstraints.flatMap(dc => columns.map(col => dc[col]));
+
+    const query = `INSERT INTO constrainstdemos_backup (${columns.join(',')}) VALUES ${placeholders}`;
+
+    return await executeDev7(query, values);
+}
+
+async function insertStudiesStatusCountArchive(statusCounts) {
+    if (!statusCounts || statusCounts.length === 0) return;
+
+    const columns = Object.keys(statusCounts[0]);
+
+    const placeholders = statusCounts
+        .map(() => `(${columns.map(() => '?').join(',')})`)
+        .join(',');
+
+    const values = statusCounts.flatMap(sc => columns.map(col => sc[col]));
+
+    // Update only some fields if duplicate _id exists
+    const updateColumns = columns.filter(col => col !== '_id').map(col => `${col} = VALUES(${col})`).join(', ');
+
+    const query = `
+        INSERT INTO studies_status_count_backup (${columns.join(',')})
+        VALUES ${placeholders}
+        ON DUPLICATE KEY UPDATE ${updateColumns}
+    `;
+
+    return await executeDev7(query, values);
+}
+
+async function insertStudiesStatusCountOnVendorsArchive(statusCounts) {
+    if (!statusCounts || statusCounts.length === 0) return;
+
+    const columns = Object.keys(statusCounts[0]);
+
+    const placeholders = statusCounts
+        .map(() => `(${columns.map(() => '?').join(',')})`)
+        .join(',');
+
+    const values = statusCounts.flatMap(sc => columns.map(col => sc[col]));
+
+    // Update all columns except primary key (_id) if duplicate
+    const updateColumns = columns.filter(col => col !== '_id').map(col => `${col} = VALUES(${col})`).join(', ');
+
+    const query = `
+        INSERT INTO studies_status_count_on_vendor_backup (${columns.join(',')})
+        VALUES ${placeholders}
+        ON DUPLICATE KEY UPDATE ${updateColumns}
+    `;
+
+    return await executeDev7(query, values);
+}
+
+async function updateArchivedFlagInStudies(sids) {
+    let query = 'update studies set isArchived=1 where apiType=1 and _id in (?)';
+    let result = await executeDev7(query, [sids]);
+    return result;  
+
+}
 module.exports = {
     getStudiesForArchiving,
     getStudiesMappingForArchiving,
@@ -56,5 +218,15 @@ module.exports = {
     getConstraintsForArchiving,
     getConstraintsDemoMappingForArchiving,
     getStudiesStatusCountForArchiving,
-    getStudiesStatusCountOnVendorsForArchiving
+    getStudiesStatusCountOnVendorsForArchiving,
+    insertStudiesArchive,
+    get_studies_already_archived,
+    insertMappingArchive,
+    insertStudyDemoArchive,
+    insertStudyDemoAgeArchive,
+    insertConstraintsArchive,
+    insertDemoConstraintsArchive,
+    insertStudiesStatusCountArchive,
+    insertStudiesStatusCountOnVendorsArchive,
+    updateArchivedFlagInStudies
 };
