e4e2ba19d4a0a241574ec2c141063e6f16667534
Bibul6717 <bibul.dhiman@zamplia.com>
2025-09-09 17:18:57 +0530

    Revert "done"
    This reverts commit 59395e8b231991b2eba5193e4b595dce9bf68a65.

--- src/v1/archiving/studies.js
+++ src/v1/archiving/studies.js
@@ -3,7 +3,6 @@ async function archivingStudies(req, res) {
     try {
 
         archivingStudiesData(req,res);
-        res.status(200).json({ success: true });
 
     } catch (error) {
         res.status(500).json({ success: false, message: error.message || error });
