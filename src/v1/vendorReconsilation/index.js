2ba358d08f266966e6c2f9f1c8e6d4d4b7fb335b
arvind <arvind.kumar@zamplia.com>
2026-02-17 14:24:44 +0530

    Update index.js

--- src/v1/vendorReconsilation/index.js
+++ src/v1/vendorReconsilation/index.js
@@ -5,16 +5,16 @@ function fmtDate(d) {
 }
 const escapeStr = s => (s || '').replace(/'/g, "\\'");
 
-async function insertVendorReconsilation() {
+async function insertVendorReconsilation(req, res) {
     try {
 
         //  const startDatel = '2025-11-15 01:00:00';
-        // const endDatel = '2026-01-15 00:00:00';
+        // const endDatel = '2025-12-15 01:00:00';
+        // const endDatel = '2026-02-15 00:00:00';
+        const dateIn = req.query.dateIn;
 
-        const now = new Date();
-        const start = now.getDate() >= 15
-            ? new Date(now.getFullYear(), now.getMonth(), 15)
-            : new Date(now.getFullYear(), now.getMonth() - 1, 15);
+        const now = new Date(dateIn);
+        const start = new Date(now.getFullYear(), now.getMonth() - 1, 15);
         const end = new Date(start.getFullYear(), start.getMonth() + 1, 15);
 
         // current month cycle
@@ -42,7 +42,7 @@ async function insertVendorReconsilation() {
         const vendorQuery = `
             SELECT _id as vendorId, vendorName, vendor_category
             FROM vendors
-            WHERE vendor_category IN ('Group A', 'Group B', 'Group C')
+            WHERE vendor_category IN ('Group A', 'Group B', 'Group C') and isactive = 1;
         `;
         const vendors = await execute(vendorQuery);
         if (!vendors || vendors.length === 0) return [];
@@ -50,11 +50,10 @@ async function insertVendorReconsilation() {
         const vendorMap = new Map(vendors.map(v => [v.vendorId, v]));
         const vendorIdsList = vendors.map(v => `'${v.vendorId}'`).join(',');
 
-        // calibr8 scores (avg) -> map vendorId::langCode -> avgScore
         // const calibr8Result = [];
         const calibr8Result = await getCalibr8ScoreByGroup(vendors);
         const calibr8Map = (calibr8Result || []).reduce((m, r) => {
-            m[`${r.vendorId}::${r.langCode}`] = Number(r.avgScore) || 0;
+            m[`${r.vendorId}`] = Number(r.avgScore) || 0;
             return m;
         }, {});
 
@@ -62,7 +61,6 @@ async function insertVendorReconsilation() {
         const reconQuery = `
             SELECT 
                 p.tid AS vendorId,
-                s.lang_code,
                 COUNT(p.p_id) AS TotalParticipants,
                 SUM(CASE WHEN (p.status = 1) THEN 1 ELSE 0 END) AS TotalCompletes,
                 SUM(CASE WHEN (p.status = 1) THEN p.cpi ELSE 0 END) AS Revenue,
@@ -75,20 +73,19 @@ async function insertVendorReconsilation() {
                 AND p.createdAt >= '${startDate}'
                 AND p.createdAt < '${endDate}'
                 AND s.apitype = 1
-            GROUP BY p.tid, s.lang_code
+            GROUP BY p.tid
         `;
         const reconResults = await execute(reconQuery);
         const surveeyParticipantsQuery = `
             SELECT 
                 p.vid AS vendorId,
-                survey_country as lang_code,
                 COUNT(p.id) AS TotalParticipants
             FROM survey_participants p
             WHERE p.vid IN (${vendorIdsList})
                 AND p.created_At >= '${startDate}'
                 AND p.created_At < '${endDate}'
                 AND p.is_api_survey = 1
-            GROUP BY p.vid, survey_country
+            GROUP BY p.vid
         `;
         const totalUser = await execute(surveeyParticipantsQuery);
 
@@ -96,16 +93,15 @@ async function insertVendorReconsilation() {
         const inserts = [];
         for (const row of (reconResults || [])) {
 
-            let surveyParticipants = totalUser.find(t => t.vendorId === row.vendorId && t.lang_code === row.lang_code);
+            let surveyParticipants = totalUser.find(t => t.vendorId === row.vendorId);
             let surveyParticipantsCount = surveyParticipants ? Number(surveyParticipants.TotalParticipants) : 0;
 
-            if (!surveyParticipantsCount || !(row.lang_code)) {
+            if (!surveyParticipantsCount) {
                 continue
             }
 
             const vendorId = row.vendorId;
-            const lang_code = row.lang_code || '';
-            const key = `${vendorId}::${lang_code}`;
+            const key = `${vendorId}`;
             const calibr8Score = calibr8Map[key] || 0;
             const vendor = vendorMap.get(vendorId) || {};
             const vendorName = escapeStr(vendor.vendorName || '');
@@ -127,14 +123,14 @@ async function insertVendorReconsilation() {
                 threshold = 95;
             }
 
-            inserts.push(`('${vendorId}','${vendorName}','${vendorCategory}',${TotalParticipants},${Revenue},${Expense},'${lang_code}',${Number(calibr8Score)},${TotalCompletes}, ${PositiveReconciliation}, ${NegativeReconciliation},
-                ${threshold}, '${endDate}')`);
+            inserts.push(`('${vendorId}','${vendorName}','${vendorCategory}',${TotalParticipants},${Revenue},${Expense},${Number(calibr8Score)},${TotalCompletes}, ${PositiveReconciliation}, ${NegativeReconciliation},
+                ${threshold}, '${endDate}', '${endDate}')`);
         }
 
         if (inserts.length > 0) {
             const insertQuery = `
                 INSERT INTO vendor_category_summary
-                    (tid, vendorName, vendorCategory, TotalParticipants, Revenue, Expense, lang_code, calibar8_score, TotalComplete, PositiveReconciliation, NegativeReconciliation, threshold, created_at)
+                    (tid, vendorName, vendorCategory, TotalParticipants, Revenue, Expense, calibar8_score, TotalComplete, PositiveReconciliation, NegativeReconciliation, threshold, created_at, updatedAt)
                 VALUES ${inserts.join(',')}
             `;
             await execute(insertQuery);
@@ -144,7 +140,7 @@ async function insertVendorReconsilation() {
         const vendorCategorySummaryQuery = `
             SELECT 
                 id, tid, vendorName, vendorCategory, TotalParticipants, Revenue, Expense, TotalComplete, 
-                PositiveReconciliation, NegativeReconciliation, lang_code, calibar8_score, manualCalibr8Score, created_at, updatedAt, currentMonthRecon, lastMonthRecon, olderMonthRecon, threshold
+                PositiveReconciliation, NegativeReconciliation, calibar8_score, manualCalibr8Score, created_at, updatedAt, currentMonthRecon, lastMonthRecon, olderMonthRecon, threshold
             FROM vendor_category_summary
             WHERE created_at = '${endDate}'
         `;
@@ -152,7 +148,7 @@ async function insertVendorReconsilation() {
 
         const vendorCategorySummaryQueryLast = `
             SELECT 
-                tid, lang_code, currentMonthRecon
+                tid, currentMonthRecon
             FROM vendor_category_summary
             WHERE created_at = '${lastEndDate}'
         `;
@@ -160,7 +156,7 @@ async function insertVendorReconsilation() {
 
         const vendorCategorySummaryQueryOlder = `
             SELECT 
-                tid, lang_code, lastMonthRecon
+                tid, currentMonthRecon
             FROM vendor_category_summary
             WHERE created_at = '${olderEndDate}'
         `;
@@ -180,10 +176,14 @@ async function insertVendorReconsilation() {
             const TotalComplete = Number(s.TotalComplete) || 0;
             const NegativeReconciliation = Number(s.NegativeReconciliation) || 0;
             const currentMonthRecon = Number(s.currentMonthRecon) || 0;
-            let lastMonthReconValue = vendorCategorySummaryLast.find(v => v.tid === s.tid && v.lang_code === s.lang_code);
-            let olderMonthReconValue = vendorCategorySummaryOlder.find(v => v.tid === s.tid && v.lang_code === s.lang_code);
+            let lastMonthReconValue = vendorCategorySummaryLast.find(v => v.tid === s.tid);
+            let olderMonthReconValue = vendorCategorySummaryOlder.find(v => v.tid === s.tid);
+            if (olderMonthReconValue) {
+                console.log(olderMonthReconValue);
+                
+            }
             const lastMonthRecon = Number(lastMonthReconValue ? lastMonthReconValue.currentMonthRecon : 0) || 0;
-            const olderMonthRecon = Number(olderMonthReconValue ? olderMonthReconValue.olderMonthRecon : 0) || 0;
+            const olderMonthRecon = Number(olderMonthReconValue ? olderMonthReconValue.currentMonthRecon : 0) || 0;
             const threshold = Number(s.threshold) || 0;
             if (!TotalComplete) {
                 bulkUpdates.reconciliation.push({
@@ -279,21 +279,25 @@ async function insertVendorReconsilation() {
                 }
 
                 await execute(`
-            UPDATE vendor_category_summary
-            SET ${vendorNameSet}
-                threshold = CASE ${thresholdCases} ELSE threshold END
-            WHERE id IN (${ids})
-        `);
+                    UPDATE vendor_category_summary
+                    SET ${vendorNameSet}
+                        threshold = CASE ${thresholdCases} ELSE threshold END
+                    WHERE id IN (${ids})
+                `);
             }
         }
 
         console.log('All thing is done');
 
 
-        return vendors;
+        return res.send({
+                success: true,
+            })
     } catch (error) {
-        console.error(error);
-        throw error;
+        return res.send({
+                success: false,
+                error: "error"
+            })
     }
 }
 
@@ -301,15 +305,10 @@ const moveVendorGroup = async (
     vendorId,
     fromGroup,
     toGroup,
-    actionType,
-    performedBy,
 ) => {
     try {
-        const query = `UPDATE vendor_category_summary SET vendorCategory = ? WHERE tid = ?`;
-        const updateResult = (await execute(query, [toGroup, vendorId]));
-        if (!updateResult?.affectedRows) {
-            return { success: false, message: "Failed to move vendor!" };
-        }
+        // const query = `UPDATE vendors SET vendor_category = ? WHERE _id = ?`;
+        // await execute(query, [toGroup, vendorId]);
         const logQuery = `INSERT INTO vendor_category_summary_log (vendorId, fromGroup, toGroup, actionType, performedBy) VALUES (?, ?, ?, ?, ?)`;
         await execute(logQuery, [vendorId, fromGroup, toGroup, 'Vendor Group Move', 'SYSTEM']);
 
@@ -328,6 +327,7 @@ async function getCalibr8ScoreByGroup(vendors) {
         // Step 1: Get vendors
 
         if (!vendors || vendors.length === 0) return [];
+        vendors.length = 2
         // Step 2: Date range
         const now = new Date();
         const start = now.getDate() >= 15
@@ -356,12 +356,10 @@ async function getCalibr8ScoreByGroup(vendors) {
 
                 const query = `
                     SELECT 
-                        s.lang_code AS langCode,
                         cs.vid AS vendorId,
                         cs.overall_score AS score
                     FROM project_calibr8_response cs
-                    INNER JOIN studies s ON s._id = cs.sid
-                    WHERE s.apitype = 1 AND cs.created_at >= '${chunkStart}'
+                    WHERE cs.created_at >= '${chunkStart}'
                         AND cs.created_at < '${chunkEnd}'
                         AND cs.vid IN (${vendorIdsList})
                         AND cs.overall_score IS NOT NULL
@@ -371,11 +369,10 @@ async function getCalibr8ScoreByGroup(vendors) {
 
                 if (rows && rows.length > 0) {
                     for (const row of rows) {
-                        const key = `${row.langCode}::${row.vendorId}`;
+                        const key = `${row.vendorId}`;
 
                         if (!aggregatedMap[key]) {
                             aggregatedMap[key] = {
-                                langCode: row.langCode,
                                 vendorId: row.vendorId,
                                 totalScore: 0,
                                 count: 0
@@ -391,14 +388,13 @@ async function getCalibr8ScoreByGroup(vendors) {
 
         // Step 5: Calculate averages
         const results = Object.values(aggregatedMap).map(entry => ({
-            langCode: entry.langCode,
             vendorId: entry.vendorId,
             avgScore: entry.count > 0
                 ? parseFloat((entry.totalScore / entry.count).toFixed(2))
                 : 0
         }));
 
-        console.log(`Final results: ${results.length} lang-vendor combinations`);
+        console.log(`Final results: ${results.length} vendor combinations`);
         return results;
 
     } catch (err) {
@@ -420,7 +416,7 @@ function getWeeklyChunks(startDate, endDate) {
     let current = new Date(startDate);
     // const WEEK_MS = 7 * 24 * 60 * 60 * 1000;
 
-    const DAYS = 31;
+    const DAYS = 7;
     const CHUNK_MS = DAYS * 24 * 60 * 60 * 1000;
 
     while (current < endDate) {
